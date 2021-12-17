28d088a273b776d7e78f698b5cac99a4fd8e1705
= <=>
2021-12-17 11:43:09 -0600

    created nav test

---
+++ src/components/Nav/__test__/index.test.js
@@ -0,0 +1,8 @@
+//import react
+import React from "react";
+//import elements from testing library
+import { render,cleanup } from "@testing-library/react";
+// import jest-dom
+import '@testing-library/jest-dom/extend-expect';
+//imports Nav from same folder
+import Nav from '..';
