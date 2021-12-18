284679c6be37a9d78a274a34caea944c5c16f977
= <=>
2021-12-17 20:34:25 -0600

    created contact component

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -4,6 +4,7 @@ import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Gallery(props) {
   const { currentCategory } = props;
+  //create DOM elements
   return (
     <section>
       <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
