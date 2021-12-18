5161ee6ba6ea79f60fe0fb68cea874c104484bb2
= <=>
2021-12-17 19:45:29 -0600

    updated gallery

--- src/components/Gallery/__test__/index.test.js
+++ src/components/Gallery/__test__/index.test.js
@@ -1,25 +1,23 @@
-// __tests__/Gallery.test.js
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
-import Gallery from '..'
-const portrait = { name: "portraits", description: "Portraits of people in my life" };
+import Gallery from '..';
+const portrait = { name: " portraits", description: "Portraits of people in my life" };
 
 afterEach(cleanup)
 
 describe('Gallery component', () => {
+    it('renders', () => {
+        render(<Gallery currentCategory={portrait} />);
+    });
 
-  it('renders', () => {
-    render(<Gallery currentCategory={portrait} />);
-  });
-
-  it('matches snapshot', () => {
-    const { asFragment } = render(<Gallery currentCategory={portrait} />)
-    expect(asFragment()).toMatchSnapshot()
-  })
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Gallery currentCategory={portrait} />)
+        expect(asFragment()).toMatchSnapShot()
+    });
 });
 
 it('renders', () => {
-  const { getByTestId } = render(<Gallery currentCategory={portrait} />)
-  expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
+    const { getByTestId } = render(<Gallery currentCategory={portrait} />)
+    expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
 })
